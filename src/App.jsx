0e94f12c39a6312eb8c64a555e58db06d8b4802d
aquicor <aquinoacordero@gmail.com>
2023-02-24 21:21:23 +0100

    Programs and Reasons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Hero from './components/Hero/Hero';
+import Programs from './components/Programs/Programs';
 
 import './App.css';
+import Reasons from './components/Reasons/Reasons';
 
 function App() {
   return (
     <div className="App">
-      <Hero/>
+      <Hero />
+      <Programs />
+      <Reasons />
     </div>
   );
 }
---
+++ src/components/Programs/Programs.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import rightArrow from '../../assets/rightArrow.png';
+
+import './Programs.css';
+
+import { programsData } from '../../data/programsData';
+
+export default function Programs() {
+  return (
+    <div className='programs' id='programs'>
+      <div className="programs-header">
+        <span className='stroke-text'>Explore</span>
+        <span>Programs</span>
+        <span className='stroke-text'>to shape you</span>
+      </div>
+
+      <div className="program-categories">
+        {
+          programsData.map((program) => (
+            <div className="category">
+              {program.image}
+              <span>{program.heading}</span>
+              <span>{program.details}</span>
+              <div className="join-now"><span>Join Now</span><img src={rightArrow} alt="RightArrow"/></div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  )
+}
