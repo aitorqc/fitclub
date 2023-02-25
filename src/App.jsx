8e2ebb19174bde03daea3f8aac0afb3a4724edca
aquicor <aquinoacordero@gmail.com>
2023-02-25 13:20:41 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
 import Plans from './components/Plans/Plans';
 import Join from './components/Join/Join';
+import Testimonials from './components/Testimonials/Testimonials';
+import Footer from './components/Footer/Footer';
 
 import './App.css';
-import Testimonials from './components/Testimonials/Testimonials';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Plans />
       <Testimonials />
       <Join />
+      <Footer />
     </div>
   );
 }
